06aafde3873dc95ec87d0f6c73f61f61a7fd122d
progress <leswifil@gmail.com>
2023-04-25 17:15:59 +0200

    first widget test

--- calculateBill.js
+++ calculateBill.js
@@ -1,12 +1,9 @@
 function calculateBill (){
     //string input
     var stringInput;
-    //waring level
-    var warning = 20
-    //danger level
-    var danger = 30
-    //split the string
-    var splitSting;
+    var splitString;
+    var call = 2.75;
+    var sms = 0.75;
 
     // get the text from the textarea in the DOM
     function setStringInput(string){
@@ -16,9 +13,58 @@ function calculateBill (){
     function getStringInput(){
         return stringInput
     }
+    //function to split the entered string
+    function getSplitString(){
+        splitString = stringInput.split(', ')
+        return splitString
+    }
+    //get total based on string input
+    function getTotal(){
+        let total = 0
+        for(let i = 0; i < splitString.length; i++){
+            if(splitString[i].trim() === "call"){
+                total += 2.75
+            }
+            else if(splitString[i].trim === "sms"){
+                total += 0.75
+            }
+        }
+        return total.toFixed(2);
+    }
+
+    //add warning and danger classes
+    function addWarning(){
+        if(getTotal() >= 20){
+            return "warning"
+        }
+    }
+    function addDanger(){
+        if(getTotal() >= 30){
+            return "danger"
+        }
+    }
+
+    //remove warning and danger classes
+
+    function removeWarning(){
+        if(getTotal() < 20){
+            return "warning"
+        }
+    }
+    function removeDanger(){
+        if(getTotal() < 30){
+            return "danger"
+        }
+    }
 
     return {
         setStringInput,
-        getStringInput
+        getStringInput,
+        getSplitString,
+        getTotal,
+        addWarning,
+        addDanger,
+        removeWarning,
+        removeDanger,
     }
 }
--- test/calculateBill.test.js
+++ test/calculateBill.test.js
@@ -1,7 +1,61 @@
 describe ("calculateBill", function(){
-    it("should  be able to set the string input", function(){
-        let billOne = calculateBill();
-        billOne.setStringInput("call");
-        assert.equal("call", billOne.getStringInput())
+    //test for string input
+    describe ("getStringInput", function (){
+        it("should  be able to set the string entered", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, sms, call, sms");
+            assert.equal("call, sms, call, sms", billOne.getStringInput())
+        })
+    })
+    //test for split string 
+    describe ("getSplitString", function(){
+        it("should  be able to split the string entered by user", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, call, call, sms");
+            assert.deepEqual("call", "call", "call", "sms", billOne.getSplitString())
+        })
+    })
+    //test for total
+    describe ("getTotal", function(){
+        it("should be able to should calculate total for the string input", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, call, call");
+            billOne.getSplitString();
+            assert.equal(8.25, billOne.getTotal())
+        })
+    })
+    //test for adding classes
+    describe ("addClasses", function(){
+        it("should add danger for total starting from 20 and above", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, call, call, call, call, call, call, call, sms, sms");
+            billOne.getSplitString();
+            billOne.getTotal()
+            assert.equal("warning", billOne.addWarning())
+        })
+        it("should add  danger for total starting from 30 and above", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, call, call, call, call, call, call, call, sms, sms, call, call, call");
+            billOne.getSplitString();
+            billOne.getTotal()
+            assert.equal("danger", billOne.addDanger())
+        })
+    })
+    //test for removing classes
+    describe ("removeClasses", function(){
+        it("should remove danger for total below 20", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, call, call, call, sms, sms");
+            billOne.getSplitString();
+            billOne.getTotal()
+            assert.equal("warning", billOne.removeWarning())
+        })
+        it("should remove danger for total below 30", function(){
+            let billOne = calculateBill();
+            billOne.setStringInput("call, call, sms, sms, call, sms, call");
+            billOne.getSplitString();
+            billOne.getTotal()
+            assert.equal("danger", billOne.removeDanger())
+        })
     })
 });
